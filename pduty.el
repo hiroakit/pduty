62aec4d385aa060533c8512e81b41bd610dac627
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2024-04-20 22:38:32 +0900

    Created simply project settings by the Eask

---
+++ pduty.el
@@ -0,0 +1,35 @@
+;;; pduty.el --- Access PagerDuty  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Hiroaki ENDOH
+
+;; Author: Hiroaki ENDOH <hiroakiendoh@gmail.com>
+;; Maintainer: Hiroaki ENDOH <hiroakiendoh@gmail.com>
+;; URL: https://github.com/hiroakit/pduty
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "28.1") (request "0.3.3"))
+;; Keywords: tools data convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Access PagerDuty
+;;
+
+;;; Code:
+
+(provide 'pduty)
+;;; pduty.el ends here
